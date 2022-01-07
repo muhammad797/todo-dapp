69c9edab096e9e59a42bcdc75aceabea53c36aa4
Muhammad Ali Project <94830929+muhammad-ali-toptal@users.noreply.github.com>
2022-01-07 16:41:36 +0500

    Express server added

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+app.use(express.static('app'));
+
+app.use(express.static('build/contracts'));
+
+app.get('/', (_, res) => res.send(`${__dirname}/app/index.html`));
+
+app.get('*', (_, res) => res.status(404).send('Page not found'));
+
+app.listen(PORT, () => {
+  console.log(`ETB Ethereum ToDo List App running on Port ${PORT}`);
+});
